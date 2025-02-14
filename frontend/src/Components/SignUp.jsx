9a85bbab50f6e7b9068e03c0fe06148a1dde3311
diagnoAI <afreedmohamed22sit36@gmail.com>
2025-02-15 00:53:19 +0530

    database added

--- frontend/src/Components/SignUp.jsx
+++ frontend/src/Components/SignUp.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; 
 
 function SignUp() {
   const [fullname, setFullname] = useState("");
